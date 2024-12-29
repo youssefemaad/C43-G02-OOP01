d8d8b029b0868d0899a5008142551f3bcd3728b2
Youssef Emad <eyoussef228@gmail.com>
2024-12-29 23:52:39 +0200

    new

--- Assignment_5/obj/Debug/net9.0/Assignment_5.AssemblyInfo.cs
+++ Assignment_5/obj/Debug/net9.0/Assignment_5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment_5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6145d4a4d4add48b9ad03e1d9523b0f96fee1f7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment_5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment_5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
