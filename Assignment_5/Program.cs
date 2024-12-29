6145d4a4d4add48b9ad03e1d9523b0f96fee1f7b
Youssef Emad <eyoussef228@gmail.com>
2024-12-29 23:37:47 +0200

    new

---
+++ Assignment_5/Program.cs
@@ -0,0 +1,29 @@
+﻿// Assignment 01 OOP
+using System;
+
+namespace Assignment01OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1. Create an Enum called "WeekDays" with the days of the week (Monday to Sunday) as its members.
+            // Write a C# program that prints out all the days of the week using this Enum.
+            #endregion
+
+            #region 2. Create an Enum called "Season" with the four seasons (Spring, Summer, Autumn, Winter) as its members.
+            // Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season.
+            #endregion
+
+            #region 3. Assign the following Permissions (Read, Write, Delete, Execute) in a form of Enum.
+            // Create a variable from the previous Enum to Add and Remove Permission from the variable.
+            // Check if a specific Permission exists inside the variable.
+            #endregion
+
+            #region 4. Create an Enum called "Colors" with the basic colors (Red, Green, Blue) as its members.
+            // Write a C# program that takes a color name as input from the user and displays a message
+            // indicating whether the input color is a primary color or not.
+            #endregion
+        }
+    }
+}
